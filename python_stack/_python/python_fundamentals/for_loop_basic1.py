3ef3cef438d1f7500afa8cfc74f6def10cdbc46c
andrewingram928 <andrewingram928@gmail.com>
2020-09-29 14:34:13 -0700

    for loop basic1

---
+++ python_stack/_python/python_fundamentals/for_loop_basic1.py
@@ -0,0 +1,35 @@
+# Basic
+# for x in range(0,151):
+#     print(x)
+
+# Multiples of Five
+# for x in range(5,1001):
+#     if x % 5 == 0:
+#         print(x)
+
+# Counting the Dojo Way
+# for x in range(0,101):
+#     if x % 10 == 0:
+#         print("Coding Dojo")
+#     elif x % 5 == 0:
+#         print("Coding")
+#     else: print(x)
+
+# Whoa.  That Sucker's Huge
+# sum = 0
+# for x in range(0,500001):
+#     if x % 2 != 0:
+#         sum = sum + x
+# print(sum)
+
+# Countdown by fours
+# for x in range(2018,0,-4):
+#     print(x)
+
+# Flexible Counter
+lowNum = 2
+highNum = 9
+mult = 3
+for x in range(lowNum, highNum+1):
+    if x % mult == 0:
+        print(x)
