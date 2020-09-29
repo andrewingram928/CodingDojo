88a09d5856c387f733213fcd7c1e09d537a96ff8
andrewingram928 <andrewingram928@gmail.com>
2020-09-29 14:35:15 -0700

    updated for loops

--- python_stack/_python/python_fundamentals/for_loop_basic1.py
+++ python_stack/_python/python_fundamentals/for_loop_basic1.py
@@ -1,30 +1,30 @@
 # Basic
-# for x in range(0,151):
-#     print(x)
+for x in range(0,151):
+    print(x)
 
 # Multiples of Five
-# for x in range(5,1001):
-#     if x % 5 == 0:
-#         print(x)
+for x in range(5,1001):
+    if x % 5 == 0:
+        print(x)
 
 # Counting the Dojo Way
-# for x in range(0,101):
-#     if x % 10 == 0:
-#         print("Coding Dojo")
-#     elif x % 5 == 0:
-#         print("Coding")
-#     else: print(x)
+for x in range(0,101):
+    if x % 10 == 0:
+        print("Coding Dojo")
+    elif x % 5 == 0:
+        print("Coding")
+    else: print(x)
 
 # Whoa.  That Sucker's Huge
-# sum = 0
-# for x in range(0,500001):
-#     if x % 2 != 0:
-#         sum = sum + x
-# print(sum)
+sum = 0
+for x in range(0,500001):
+    if x % 2 != 0:
+        sum = sum + x
+print(sum)
 
 # Countdown by fours
-# for x in range(2018,0,-4):
-#     print(x)
+for x in range(2018,0,-4):
+    print(x)
 
 # Flexible Counter
 lowNum = 2
