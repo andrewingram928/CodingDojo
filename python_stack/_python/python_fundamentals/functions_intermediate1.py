cfe36ccf8873125fa272eea051ec36bfb3814017
andrewingram928 <andrewingram928@gmail.com>
2020-10-01 17:14:04 -0700

    functions int

---
+++ python_stack/_python/python_fundamentals/functions_intermediate1.py
@@ -0,0 +1,25 @@
+import random
+def randInt(min= 0, max= 0):
+    if min == 0 and max == 0:
+        num = random.random() * 100
+        return num
+    elif min == 0:
+        num = random.random() * max
+        return num
+    elif max == 0:
+        num = random.random() * (100-min) + min
+        return num
+    elif min > 0 and max > 0:
+        if min > max:
+            num = random.random() * (min - max) + max
+            return num
+        else:
+            num = random.random() * (max-min) + min
+            return num
+x = randInt(min=500, max=50)
+print(x)
+
+#print(randInt()) 		    # should print a random integer between 0 to 100
+#print(randInt(max=50)) 	    # should print a random integer between 0 to 50
+#print(randInt(min=50)) 	    # should print a random integer between 50 to 100
+#print(randInt(min=50, max=500))    # should print a random integer between 50 and 500
