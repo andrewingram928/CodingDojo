bc1efe3887f5592a574f419e19054b858593ecee
andrewingram928 <andrewingram928@gmail.com>
2020-10-04 17:32:52 -0700

    lots

--- python_stack/_python/python_fundamentals/functions_intermediate1.py
+++ python_stack/_python/python_fundamentals/functions_intermediate1.py
@@ -19,7 +19,8 @@ def randInt(min= 0, max= 0):
 x = randInt(min=500, max=50)
 print(x)
 
-#print(randInt()) 		    # should print a random integer between 0 to 100
-#print(randInt(max=50)) 	    # should print a random integer between 0 to 50
-#print(randInt(min=50)) 	    # should print a random integer between 50 to 100
-#print(randInt(min=50, max=500))    # should print a random integer between 50 and 500
+print(randInt()) 		    # should print a random integer between 0 to 100
+print(randInt(max=50)) 	    # should print a random integer between 0 to 50
+print(randInt(min=50)) 	    # should print a random integer between 50 to 100
+print(randInt(min=50, max=500))    # should print a random integer between 50 and 500
+
