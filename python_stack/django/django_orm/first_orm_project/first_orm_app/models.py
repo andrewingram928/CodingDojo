8fe9cdac049a740ebb089814b533c8d770ee1cd1
andrewingram928 <andrewingram928@gmail.com>
2020-10-21 21:11:22 -0700

    Updates to ninja, first orm

---
+++ python_stack/django/django_orm/first_orm_project/first_orm_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Wizard(models.Model):
+    name = models.CharField(max_length=45)
+    house = models.CharField(max_length=45)
+    pet = models.CharField(max_length=45)
+    year = models.IntegerField()
+
+class Hizard(models.Model):
+    name = models.CharField(max_length=45)
+    house = models.CharField(max_length=45)
+    pet = models.CharField(max_length=45)
+    year = models.IntegerField()
