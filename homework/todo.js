d31be66b6497f2ef6dd46b2095bffff901fe2bb0
andrewingram928 <andrewingram928@gmail.com>
2020-12-21 00:15:13 -0800

    avg

--- homework/todo.js
+++ homework/todo.js
@@ -34,12 +34,15 @@ class Node {
         }
     }
     contains(value) {
-        if(value) {
-            return true;
-        }
-        else {
-            return false;
-        }
+        var current = this.head;
+        while(current !== null)
+            if(current.value == value) {
+                return true;
+            }
+            else {
+                current = current.next;
+            }
+        return false
     }
     length() {
         var current = this.head;
@@ -57,19 +60,56 @@ class Node {
             current = current.next;
         }
     }
+    max() {
+        var current = this.head;
+        var max = 0;
+        while(current !== null) {
+            if(current.value > max) {
+                max = current.value;
+                console.log(`----The max value in the list is NOW ${max}`)
+            }
+            current = current.next;
+        }
+        console.log(`!!!The max value in the list is ${max}!!!`)
+    }
+    min() {
+        var current = this.head;
+        var min = current.value;
+        while(current !== null) {
+            if(current.value < min) {
+                min = current.value;
+                console.log(`--------The min value in the list is NOW ${min}`)
+            }
+            current = current.next;
+        }
+        console.log(`!!!!!The min value in the list is ${min}!!!!!`)
   }
+  avg() {
+    var current = this.head;
+    var sum = 0;
+    var count = 0;
+    while(current !== null) {
+        sum = current.value + sum;
+        count ++;
+        current = current.next;
+    }
+    sum = sum / count;
+    console.log(sum)
+  }
+}
+var first_node = new Node(8);
+var second_node = new Node(10);
+var third_node = new Node(15);
+var fourth_node = new Node(100);
 
-  var first_node = new Node('Andrew')
-  var second_node = new Node('Anthony')
-  var third_node = new Node('Anton')
-  var fourth_node = new Node('Lewis')
-
-  var this_sll = new SLL();
-  this_sll.addFront('Andrew').addFront('Anthony').addFront('Anton').addFront('Lewis')
-  console.log(this_sll)
-  this_sll.removeFront()
-  this_sll.front()
-  console.log(this_sll.contains('Andrew'))
-  console.log(this_sll.length())
-  this_sll.display()
-
+var this_sll = new SLL();
+this_sll.addFront(100).addFront(5).addFront(-2).addFront(50).addFront(800).addFront(5).addFront(10).addFront(6)
+console.log(this_sll)
+this_sll.removeFront()
+this_sll.front()
+console.log(this_sll.contains(100))
+console.log(this_sll.length())
+this_sll.display()
+this_sll.max()
+this_sll.min()
+this_sll.avg()
